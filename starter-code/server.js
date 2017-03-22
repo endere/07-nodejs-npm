e1af6eeef278f2f5f599dca5b4bb40ded29ce45f
Kevin Schmidt <NSPtuning@gmail.com>
2017-03-22 16:37:48 -0700

    kevin-erik

--- starter-code/server.js
+++ starter-code/server.js
@@ -11,11 +11,16 @@ const PORT = process.env.PORT || 3000;
 // TODO: Include all of the static resources as an argument to app.use()
 app.use(express.static('./public'));
 app.get('*', function(request, response) {
+  response.sendFile('public/index.html', {root: '.'});
   // TODO: Using the response object, send the index.html file back to the user
 });
 
 // TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
+app.get('/new', function(request, response) {
+  response.sendFile('public/new.html', {root: '.'});
+});
 
 app.listen(PORT, function() {
   // TODO: Log to the console a message that lets you know which port your server has started on
+  console.log(`You are being served at LocalPort:${PORT}`);
 });
