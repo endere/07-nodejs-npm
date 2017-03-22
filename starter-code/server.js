f056e95649ccb7048a8038e7cc2677545cab1214
Erik Enderlein <erik.end@gmail.com>
2017-03-22 18:23:59 -0500

    first half of pair programming

--- starter-code/server.js
+++ starter-code/server.js
@@ -4,11 +4,12 @@
 
 // TODO: Require the Express package that you installed via NPM, and instantiate the app
 // Remember to install express, and be sure that it's been added to your package.json as a dependency
-
+const express = require('express');
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 // TODO: Include all of the static resources as an argument to app.use()
-
+app.use(express.static('./public'));
 app.get('*', function(request, response) {
   // TODO: Using the response object, send the index.html file back to the user
 });
